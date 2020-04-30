0a2c791b935450496aa0574c83a3562dc1090fad
Dan Nilsson <mail@dannilsson.se>
2020-04-30 22:59:15 +0200

    refactor code structure

--- app/__init__.py
+++ app/__init__.py
@@ -2,47 +2,28 @@ import os
 from flask import Flask
 from flask_restful import Api
 
-# routes
-from . import routes
-
-
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-    )
-
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
+def create_app():
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+    app.config.from_object('config.DevConfig')
 
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    @app.route('/')
-    def hello():
-        return 'Hello Dude!'
 
-    api = Api(app)
+    with app.app_context():
 
-    # Endpoints TEST
-    api.add_resource(
-        routes.Students,
-        '/students/'
-    )
+        # routes
+        from . import routes
 
-    api.add_resource(
-        routes.Student,
-        '/students/<string:student_id>'
-    )
+        api = Api(app)
 
-    # Viva
-    api.add_resource(
-        routes.viva.MyPages,
-        '/viva/mypages/<string:hash_id>'
-    )
+        # Viva
+        api.add_resource(
+            routes.MyPages,
+            '/viva/mypages/<string:hash_id>'
+        )
 
-    return app
+        return app
