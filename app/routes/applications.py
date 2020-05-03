fbc6c2e213de8cd161070ec81b1a2357f64acce7
Dan Nilsson <mail@dannilsson.se>
2020-05-03 08:09:00 +0200

    add env/config handling

--- app/routes/applications.py
+++ app/routes/applications.py
@@ -1,3 +1,4 @@
+from flask import current_app
 from flask_restful import Resource, reqparse
 
 
--- config.py
+++ config.py
@@ -2,16 +2,8 @@ import os
 
 class Config:
   # Base
-  SALT = 'some_secret_salt'
+  SALT = os.environ.get('SALT')
   COOKIE_AUTH_NAME = os.environ.get('COOKIE_AUTH_NAME')
-
-class ProdConfig:
-  DEBUG = False
-  STAGE = 'prod'
-  WSDL_URL = os.environ.get('PROD_WSDL_URL')
-  LOGIN_POST_URL = os.environ.get('PROD_LOGIN_POST_URL')
-  LOGIN_POST_USERNAME = os.environ.get('PROD_LOGIN_POST_USERNAME')
-  LOGIN_POST_PASSWORD = os.environ.get('PROD_LOGIN_POST_PASSWORD')
 
 class DevConfig:
   DEBUG = True
@@ -20,3 +12,11 @@ class DevConfig:
   LOGIN_POST_URL = os.environ.get('DEV_LOGIN_POST_URL')
   LOGIN_POST_USERNAME = os.environ.get('DEV_LOGIN_POST_USERNAME')
   LOGIN_POST_PASSWORD = os.environ.get('DEV_LOGIN_POST_PASSWORD')
+
+class ProdConfig:
+  DEBUG = False
+  STAGE = 'prod'
+  WSDL_URL = os.environ.get('PROD_WSDL_URL')
+  LOGIN_POST_URL = os.environ.get('PROD_LOGIN_POST_URL')
+  LOGIN_POST_USERNAME = os.environ.get('PROD_LOGIN_POST_USERNAME')
+  LOGIN_POST_PASSWORD = os.environ.get('PROD_LOGIN_POST_PASSWORD')
