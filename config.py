a3a25d779702dc530ecfc18bba98bded9c893434
Dan Nilsson <mail@dannilsson.se>
2020-05-04 01:35:32 +0200

    refactor config using json

--- config.py
+++ config.py
@@ -1,17 +1,20 @@
 import os
 
 class Config:
-  # Base
   SALT = os.environ.get('SALT')
-  COOKIE_AUTH_NAME = os.environ.get('COOKIE_AUTH_NAME')
+  COOKIE_AUTH_NAME = os.environ.get('COOKIE_AUTH_NAME'),
 
 class DevConfig:
   DEBUG = True
   STAGE = 'dev'
-  WSDL_URL = os.environ.get('DEV_WSDL_URL')
-  LOGIN_POST_URL = os.environ.get('DEV_LOGIN_POST_URL')
-  LOGIN_POST_USERNAME = os.environ.get('DEV_LOGIN_POST_USERNAME')
-  LOGIN_POST_PASSWORD = os.environ.get('DEV_LOGIN_POST_PASSWORD')
+  VIVA = {
+      'wsdl_url': os.environ.get('DEV_WSDL_URL'),
+      'login': {
+          'url': os.environ.get('DEV_LOGIN_POST_URL'),
+          'username': os.environ.get('DEV_LOGIN_POST_USERNAME'),
+          'password': os.environ.get('DEV_LOGIN_POST_PASSWORD'),
+      },
+  }
 
 class ProdConfig:
   DEBUG = False
