110a5950fadd1cde384f2dd6dc5d43d2a229547b
Dan Nilsson <mail@dannilsson.se>
2020-05-04 01:36:50 +0200

    add post method to application route

--- app/routes/applications.py
+++ app/routes/applications.py
@@ -1,10 +1,28 @@
-from flask import current_app
+from flask import current_app, jsonify
 from flask_restful import Resource, reqparse
 
+from ..libs.viva_application import VivaApplication
+
+parser = reqparse.RequestParser()
+parser.add_argument('type', required=True)
+parser.add_argument('application', type=dict, required=True)
 
 class Applications(Resource):
-    def get(self, pnr):
-        if pnr is None:
-            return 'Not found', 404
-        else:
-            return 'APPLICATIONS LIST'
+    def get(self):
+        return 'APPLICATIONS'
+
+    def post(self):
+        json_data = parser.parse_args()
+
+        vivaAppli = VivaApplication(
+            usr='19760819T1234',
+            pnr='19760819T1234'
+        )
+
+        response = vivaAppli.new_re_application(
+            key=123,
+            period={},
+            re_application={ 'some': 'value' }
+        )
+
+        return jsonify(response)
