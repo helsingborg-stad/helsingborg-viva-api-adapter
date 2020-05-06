faa48eaca4acf3a4484710fb5ee9dbf4f31cd128
Dan Nilsson <mail@dannilsson.se>
2020-05-06 10:41:17 +0200

    add cookie test route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,2 +1,3 @@
 from .my_pages import MyPages
 from .applications import Applications
+from .testcookie import TestCookie
---
+++ app/routes/testcookie.py
@@ -0,0 +1,15 @@
+from flask import current_app
+from flask_restful import Resource
+
+from ..libs.session import Session
+
+
+class TestCookie(Resource):
+  def get(self):
+    sess = Session()
+    trans = sess.get_transport()
+
+    return {
+      'SessionCookie': str(Session._cookie),
+      'transportSessionCookie': str(trans.session.cookies),
+    }, 200
