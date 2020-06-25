20451e64c26aaeff404a363735096207cc4d37d5
Dan Nilsson <mail@dannilsson.se>
2020-06-25 22:35:57 +0200

    add settings to client obj
    set strict to false
    set xml_huge_tree to ture

--- app/libs/viva.py
+++ app/libs/viva.py
@@ -1,20 +1,26 @@
 import zeep.helpers as zeep_helpers
 from flask import current_app
-from zeep import Client
+from zeep import Client, Settings
 
 from .session import Session
 
 
 class Viva(object):
 
-    def __init__(self, session=Session, current_app=current_app, zeep_helpers=zeep_helpers):
+    def __init__(self,
+        session=Session,
+        settings=Settings,
+        current_app=current_app,
+        zeep_helpers=zeep_helpers
+    ):
         self._config = current_app.config['VIVA']
         self._helpers = zeep_helpers
         self._session = session()
+        self._settings = Settings(strict=False, xml_huge_tree=True)
 
     def _get_service(self, wsdl):
         transport = self._session.get_transport()
         wsdl_url = self._config['wsdl_url'] + '/' + wsdl + '?WSDL'
-        client = Client(wsdl=wsdl_url, transport=transport)
+        client = Client(wsdl=wsdl_url, transport=transport, settings=self._settings)
 
         return client.service
