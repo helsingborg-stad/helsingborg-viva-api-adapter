f1582a40d7eba6733e5a0faf2d0599008b2aa5e1
Dan Nilsson <mail@dannilsson.se>
2020-09-07 21:39:32 +0200

    add zeep settings

--- app/libs/viva.py
+++ app/libs/viva.py
@@ -1,6 +1,6 @@
 import zeep.helpers as zeep_helpers
 from flask import current_app
-from zeep import Client
+from zeep import Client, Settings
 
 from .session import Session
 
@@ -8,10 +8,12 @@ from .session import Session
 class Viva(object):
 
     def __init__(self,
+        settings=Settings,
         session=Session,
         current_app=current_app,
         zeep_helpers=zeep_helpers
     ):
+        self._settings = settings(strict=False, xml_huge_tree=True)
         self._config = current_app.config['VIVA']
         self._helpers = zeep_helpers
         self._session = session()
@@ -19,6 +21,6 @@ class Viva(object):
     def _get_service(self, wsdl):
         transport = self._session.get_transport()
         wsdl_url = self._config['wsdl_url'] + '/' + wsdl + '?WSDL'
-        client = Client(wsdl=wsdl_url, transport=transport)
+        client = Client(wsdl=wsdl_url, transport=transport, settings=self._settings)
 
         return client.service
