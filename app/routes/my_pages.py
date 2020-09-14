9f6bf40bcd575d26707fa6b310cf357e810f1942
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-09-14 16:58:39 +0200

    refactor exception
    fault.detail caused wiered response error

--- app/routes/my_pages.py
+++ app/routes/my_pages.py
@@ -33,9 +33,6 @@ class MyPages(Resource):
 
         except Fault as fault:
             return {
-                'fault': {
-                    'message': fault.message,
-                    'detail': fault.detail,
-                    'code': fault.code,
-                }
+                'message': fault.message,
+                'code': fault.code
             }, 500
