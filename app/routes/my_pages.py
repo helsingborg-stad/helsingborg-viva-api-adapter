d76ce324e5154b45514aac0c3b87588c56f7cebe
Dan Nilsson <mail@dannilsson.se>
2020-09-16 00:19:47 +0200

    add person application viva operation
    refactor casessi into own method

--- app/routes/my_pages.py
+++ app/routes/my_pages.py
@@ -22,12 +22,15 @@ class MyPages(Resource):
                 'person': {
                     'info': my_pages.person_info['vivadata'],
                     'cases': my_pages.person_cases['vivadata'],
-                    'booked_payments': my_pages.get_person_booked_payments['vivadata'],
+                    'booked_payments': my_pages.person_booked_payments['vivadata'],
                 }
             }
 
             if my_pages.person_caseworkflow is not False:
                 response['person']['caseworkflow'] = my_pages.person_caseworkflow['vivadata']
+
+            if my_pages.person_application is not False:
+                response['person']['application'] = my_pages.person_application['vivadata']
 
             return response, 200
 
