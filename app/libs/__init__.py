28942504340d3a71e8e4b9d951dc65f2715b6e82
Dan Nilsson <mail@dannilsson.se>
2020-09-22 08:23:28 +0200

    add make pnr to test format

--- app/libs/__init__.py
+++ app/libs/__init__.py
@@ -1,3 +1,12 @@
-from .hashids import hashids
+import re
+
+
+from .hashids import parse_hash
 from .viva_application import VivaApplication
 from .my_pages import MyPages
+
+
+def make_test_pnr(pnr):
+    regex = re.compile('([0-9]{8})([0-9]{4})')
+    parts = regex.match(pnr).groups()
+    return 'T'.join(parts)
