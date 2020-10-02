8ffb7cd0c8eaea4701fd324b8fbca642b122d59d
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-10-02 14:51:46 +0200

    refactor applications route
    Put parse data to helper function located in libs folder

--- app/libs/__init__.py
+++ app/libs/__init__.py
@@ -1,3 +1,4 @@
 from .personal_number_helper import hashids_instace, decode_hash_personal_number, make_test_personal_number
 from .viva_application import VivaApplication
 from .my_pages import MyPages
+from .applications_helper import parse_application_data
--- app/routes/applications.py
+++ app/routes/applications.py
@@ -2,42 +2,8 @@ from flask import jsonify, request, current_app
 from marshmallow import ValidationError
 from flask_restful import Resource
 
-from ..libs import VivaApplication, decode_hash_personal_number, make_test_personal_number
+from ..libs import VivaApplication, decode_hash_personal_number, make_test_personal_number, parse_application_data
 from ..schemas import ApplicationSchema, ResponseSchema
-
-
-cost_types = ['EXPENSES', 'INCOMES']
-
-
-def parse_application_data(data, period, parent_key=None, initial_data=dict):
-    for key, value in data.items():
-        if parent_key == None and type(value) is dict and key.upper() in cost_types:
-            # create a new key on the initial_data param and call this function again to create the value for the key.
-            key = key.upper()  # transform the key to uppercase to match zeep viva json object
-            initial_data[key] = parse_application_data(
-                value, period, parent_key=key, initial_data=[]
-            )
-
-        if parent_key in cost_types and type(value) is dict:
-            # create a single income or expense and add it to the array passed thorugh initial_data.
-            applies_to = 'coapplication' if 'Partner' in key else 'applicant'
-            period_string = f'{period["start_date"]} - {period["end_date"]}'
-
-            initial_data.append(
-                {
-                    parent_key[:-1]: {
-                        'TYPE': key,
-                        'APPLIESTO': applies_to,
-                        'PERIOD': period_string,
-                        'FREQUENCY': 12,
-                        'AMOUNT': value['amount'],
-                        'DATE': value['date'],
-                        'DESCRIPTION': '',
-                    }
-                }
-            )
-
-    return initial_data
 
 
 class Applications(Resource):
