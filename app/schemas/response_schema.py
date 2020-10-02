d9af03c644e536aa7f5cf7893d079acb81af308c
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-10-02 13:32:36 +0200

    add allow none for error params in response schema
    We can’t force what we can’t controll

--- app/schemas/response_schema.py
+++ app/schemas/response_schema.py
@@ -3,8 +3,8 @@ from marshmallow_jsonapi import fields
 
 
 class ResponseSchema(SimpleSchema):
-    errorCode = fields.String(data_key='ERRORCODE')
-    errorMessage = fields.String(data_key='ERRORMESSAGE')
-    status = fields.String(data_key='STATUS')
+    errorCode = fields.String(data_key='ERRORCODE', allow_none=True)
+    errorMessage = fields.String(data_key='ERRORMESSAGE', allow_none=True)
+    status = fields.String(data_key='STATUS', allow_none=True)
     idenclair = fields.String(data_key='IDENCLAIR', allow_none=True)
     id = fields.String(data_key='ID', allow_none=True)
