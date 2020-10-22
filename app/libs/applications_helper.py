ce8845e17357eaee971e64cb6afdb9ca4015c139
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-10-22 22:36:09 +0200

    work in progress

--- app/libs/applications_helper.py
+++ app/libs/applications_helper.py
@@ -1,32 +1,30 @@
 cost_types = ['EXPENSES', 'INCOMES']
 
 
-def parse_application_data(data, period, parent_key=None, initial_data=dict):
-    for key, value in data.items():
-        if parent_key == None and type(value) is dict and key.upper() in cost_types:
-            # create a new key on the initial_data param and call this function again to create the value for the key.
-            key = key.upper()  # transform the key to uppercase to match zeep viva json object
-            initial_data[key] = parse_application_data(
-                value, period, parent_key=key, initial_data=[]
-            )
+def parse_application_data(answers, period_string=str):
+    data = dict()
 
-        if parent_key in cost_types and type(value) is dict:
-            # create a single income or expense and add it to the array passed thorugh initial_data.
-            applies_to = 'coapplication' if 'Partner' in key else 'applicant'
-            period_string = f'{period["start_date"]} - {period["end_date"]}'
+    for answer in answers:
+        tags = answer['field']['tags']
 
-            initial_data.append(
-                {
-                    parent_key[:-1]: {
-                        'TYPE': key,
-                        'APPLIESTO': applies_to,
-                        'PERIOD': period_string,
-                        'FREQUENCY': 12,
-                        'AMOUNT': value['amount'],
-                        'DATE': value['date'],
-                        'DESCRIPTION': '',
-                    }
-                }
-            )
+        if tags in cost_types:
+            print('HEPP')
 
-    return initial_data
+            # create a single income or expense and add it to the array passed thorugh initial_data
+            # applies_to = 'coapplication' if 'Partner' in key else 'applicant'
+
+            # data.append(
+            #     {
+            #         parent_key[:-1]: {
+            #             'TYPE': key,
+            #             'APPLIESTO': applies_to,
+            #             'PERIOD': period_string,
+            #             'FREQUENCY': 12,
+            #             'AMOUNT': value['amount'],
+            #             'DATE': value['date'],
+            #             'DESCRIPTION': '',
+            #         }
+            #     }
+            # )
+
+    return data
