d1ee5c2c47afd05033684eaf4bc2b33439c02234
Dan Nilsson <mail@dannilsson.se>
2020-10-30 00:03:17 +0100

    refactor: 💡 structure

--- app/schemas/application_schema.py
+++ app/schemas/application_schema.py
@@ -7,11 +7,11 @@ class ApplicationSchema(Schema):
     application_type = fields.Str(
         data_key='applicationType',
         required=True,
-        validate=validate.OneOf(['recurrent', 'basic']),
+        validate=validate.OneOf(['recurrent', 'basic'])
     )
+    applicant = fields.Str(required=True)
     client_ip = fields.Str(data_key='clientIp', required=True)
     workflow_id = fields.Str(data_key='workflowId', required=True)
-    applicant = fields.Str(required=True)
     period = fields.Nested(PeriodSchema(), required=True)
     answers = fields.List(
         fields.Nested(AnswerSchema(), required=True),
