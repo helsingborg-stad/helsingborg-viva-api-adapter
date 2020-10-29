c93e7bb8a28d8d6cf269a6e807e11c6949d38ab3
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-10-29 16:14:24 +0100

    feat: 🎸 add schema answer

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,4 +1,4 @@
 from .application_schema import ApplicationSchema
-from .application_answer_schema import ApplicationAnswerSchema
+from .answer_schema import AnswerSchema
 from .period_schema import PeriodSchema
 from .response_schema import ResponseSchema
