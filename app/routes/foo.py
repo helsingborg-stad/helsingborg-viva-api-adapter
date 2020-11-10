e274e10edb34c26498452dacdbbeb2c788235730
Dan Nilsson <mail@dannilsson.se>
2020-11-11 00:42:21 +0100

    feat: 🎸 add poc implementation of token
    token with public key based authentication now works

--- app/routes/foo.py
+++ app/routes/foo.py
@@ -1,3 +1,4 @@
+import jwt
 from flask import jsonify, request
 from flask_restful import Resource, abort, wraps
 from marshmallow import ValidationError
@@ -7,6 +8,18 @@ from ..schemas import ApplicationSchema
 
 
 def authentication():
+    if 'X-API-Key' not in request.headers:
+        return False
+
+    token = request.headers['X-Api-Key']
+    public_key = open('jwtRS256.key.pub').read()
+
+    try:
+        jwt.decode(token, public_key, algorithms=['RS256'])
+    except Exception as e:
+        print(e)
+        return False
+
     return True
 
 def authenticate(f):
@@ -15,11 +28,10 @@ def authenticate(f):
         if not getattr(f, 'authenticate', True):
             return f(*args, **kwargs)
 
-        acct = authentication()
-        if acct:
-            return f(*args, **kwargs)
+        if not authentication():
+            abort(http_status_code=401)    
 
-        abort(http_status_code=401)
+        return f(*args, **kwargs)
 
     return wrapper
 
