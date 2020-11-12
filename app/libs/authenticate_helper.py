165511c05a12e67d54a5b817c431052c93088c26
Dan Nilsson <mail@dannilsson.se>
2020-11-12 15:56:49 +0100

    refactor: 💡 add comment
    change function name to be more descriptive

--- app/libs/authenticate_helper.py
+++ app/libs/authenticate_helper.py
@@ -2,7 +2,7 @@ import jwt
 from flask import request
 from flask_restful import abort, wraps
 
-def authentication():
+def validate_token():
     if 'X-API-Key' not in request.headers:
         return False
 
@@ -18,12 +18,21 @@ def authentication():
 
 
 def authenticate(f):
+    """
+    Decorator function to validate token on route resources
+
+    Example:
+    class Applications(Resource):
+        method_decorators = [authenticate]
+        ...
+        ...
+    """
     @wraps(f)
     def wrapper(*args, **kwargs):
         if not getattr(f, 'authenticate', True):
             return f(*args, **kwargs)
 
-        if not authentication():
+        if not validate_token():
             abort(http_status_code=401)
 
         return f(*args, **kwargs)
