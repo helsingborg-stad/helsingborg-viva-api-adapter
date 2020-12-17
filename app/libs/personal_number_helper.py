c2235a39299cf2dbf5d9fe31d5d891cc5bbf70f8
Dan Nilsson <mail@dannilsson.se>
2020-12-17 01:20:19 +0100

    refactor: 💡 remove function, rename function

--- app/libs/personal_number_helper.py
+++ app/libs/personal_number_helper.py
@@ -1,21 +1,21 @@
 import re
-
-from flask import current_app
 from hashids import Hashids
 
+from flask import current_app
 
-hashids_instace = Hashids(
-    salt=current_app.config['SALT'],
-    min_length=32
-)
+hashids_instace = Hashids(salt=current_app.config['SALT'], min_length=32)
 
 
 def hash_to_personal_number(hash_id=str):
     personal_number = str(hashids_instace.decode(hash_id)[0])
+
+    if current_app.config['ENV'] in ['development', 'test']:
+        personal_number = to_test_personal_number(personal_number)
+
     return personal_number
 
 
-def get_test_personal_number(personal_number=str):
+def to_test_personal_number(personal_number=str):
     regex = re.compile('([0-9]{8})([0-9]{4})')
     parts = regex.match(personal_number).groups()
 
@@ -28,11 +28,3 @@ def get_test_personal_number(personal_number=str):
         # VIVA test format
         # yyyymmddTnnnn
         return 'T'.join(parts)
-
-
-def get_personal_number(hash_id=str):
-    personal_number = hash_to_personal_number(hash_id=hash_id)
-    if current_app.config['ENV'] == 'development' or current_app.config['ENV'] == 'test':
-        personal_number = get_test_personal_number(
-            personal_number=personal_number)
-    return personal_number
