e1fb19d7155395ed90aadf652acc9cbe39359db0
Dan Nilsson <mail@dannilsson.se>
2020-12-17 01:18:21 +0100

    refactor: 💡 slim down helper functions
    remove unused personal number helper functions

--- app/libs/__init__.py
+++ app/libs/__init__.py
@@ -1,10 +1,7 @@
 from .viva_application import VivaApplication
 from .my_pages import MyPages
 
-from .personal_number_helper import hashids_instace
-from .personal_number_helper import hash_to_personal_number
-from .personal_number_helper import get_test_personal_number
-from .personal_number_helper import get_personal_number
+from .personal_number_helper import hashids_instace, hash_to_personal_number
 
 from .datetime_helper import milliseconds_to_date_string
 from .authenticate_helper import authenticate
