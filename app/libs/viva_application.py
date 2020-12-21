35dc42b525f392bddf110d21c05bc8bb2e5b8745
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-12-21 23:08:02 +0100

    refactor: 💡 rename respons var to a better name

--- app/libs/viva_application.py
+++ app/libs/viva_application.py
@@ -29,14 +29,14 @@ class VivaApplication(Viva):
     def get_application_status(self):
         personal_number = self._my_pages.get_personal_number()
 
-        response = self._service.APPLICATIONSTATUS(
+        status_number = self._service.APPLICATIONSTATUS(
             SUSER=personal_number,
             SPNR=personal_number,
             SCASETYPE='01',  # 01 = EKB
             SSYSTEM=1
         )
 
-        return self._helpers.serialize_object(response)
+        return self._helpers.serialize_object(status_number)
 
     def _get_application(self):
         initial_application = {
