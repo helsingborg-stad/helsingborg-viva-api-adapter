136fc499288f5d77afa04790cd9a41b393c73d81
Dan Nilsson <mail@dannilsson.se>
2021-02-02 10:22:15 +0100

    feat: 🎸 add marshmallow validation

--- app/schemas/application_schema.py
+++ app/schemas/application_schema.py
@@ -1,5 +1,12 @@
-from marshmallow import validate, Schema, fields
+from marshmallow import validate, Schema, fields, ValidationError
 from .answer_schema import AnswerSchema
+
+
+# class BytesField(fields.Field):
+#     def _validate(self, value):
+#         bytes_from_list = bytes(value)
+#         if not isinstance(bytes_from_list, bytes):
+#             raise ValidationError('Invalid input type')
 
 
 class ApplicationSchema(Schema):
@@ -9,7 +16,20 @@ class ApplicationSchema(Schema):
         validate=validate.OneOf(['recurrent', 'basic'])
     )
     hashid = fields.Str(required=True)
+    workflow_id = fields.Str(
+        data_key='workflowId',
+        required=True
+    )
     answers = fields.List(
         fields.Nested(AnswerSchema(), required=True),
         required=True
+    )
+    raw_data = fields.Raw(
+        data_key='rawData',
+        required=True
+    )
+    raw_data_type = fields.Str(
+        data_key='rawDataType',
+        required=True,
+        validate=validate.OneOf(['pdf'])
     )
