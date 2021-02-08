2ccc8a0b1192cb0acecadaac32741ddc781ae599
Dan Nilsson <dan.nilsson@helsingborg.se>
2021-02-08 09:50:38 +0100

    feat: 🎸 add endpoint - get attachment

--- app/libs/classes/viva_attachments.py
+++ app/libs/classes/viva_attachments.py
@@ -4,26 +4,32 @@ from . import Viva
 
 class VivaAttachments(Viva):
 
-    def __init__(self, my_pages, wsdl='VivaAttachment', attachment=dict):
+    def __init__(self, my_pages, wsdl='VivaAttachment'):
         super(VivaAttachments, self).__init__()
 
         self._my_pages = my_pages
         self._service = self._get_service(wsdl)
 
+    def save(self, attachment=dict):
         if not isinstance(attachment, dict):
             raise ValidationError('Invalid attachment')
 
-        self.attachment = attachment
-
-    def save(self):
-        viva_soap_response = self._service.SAVEDATA(
+        viva_save_soap_response = self._service.SAVEDATA(
             SUSER=self._my_pages.get_personal_number(),
             SIP='0.0.0.0',
             STEMP='1',
-            SKEY=self.attachment['attachment_id'],
+            SKEY=attachment['attachment_id'],
             SPARENTID='',
-            SFILENAME=self.attachment['file_name'],
-            SDATA=self.attachment['file_base64']
+            SFILENAME=attachment['file_name'],
+            SDATA=attachment['file_base64']
         )
 
-        return self._helpers.serialize_object(viva_soap_response)
+        return self._helpers.serialize_object(viva_save_soap_response)
+
+    def get(self, attachment_id=str):
+        viva_get_soap_response = self._service.GETDATA(
+            SUSER=self._my_pages.get_personal_number(),
+            GKEY=attachment_id
+        )
+
+        return self._helpers.serialize_object(viva_get_soap_response)
