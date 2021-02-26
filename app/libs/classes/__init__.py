18816cde7cd43dfc2298a6335efa7d7a6f4f69ce
Dan Nilsson <dan.nilsson@helsingborg.se>
2021-02-26 16:43:16 +0100

    fix: 🐛 order issue

--- app/libs/classes/__init__.py
+++ app/libs/classes/__init__.py
@@ -1,5 +1,5 @@
 from .session import Session
 from .viva import Viva
 from .viva_my_pages import VivaMyPages
-from .viva_application import VivaApplication
 from .viva_attachments import VivaAttachments
+from .viva_application import VivaApplication
