34872cbc1a8ebdca91adf438a13e4c79f102bafa
Dan Nilsson <dan.nilsson@helsingborg.se>
2021-02-26 16:46:36 +0100

    fix: 🐛 implement completion route

--- app/routes/completions.py
+++ app/routes/completions.py
@@ -3,39 +3,42 @@ from flask_restful import Resource
 from zeep.exceptions import Fault
 from marshmallow import ValidationError
 
+from ..libs import VivaMyPages
+from ..libs import VivaAttachments
+from ..libs import VivaApplication
 from ..libs import hash_to_personal_number
 from ..libs import authenticate
 
-from ..schemas import CompletionSchema, ResponseSchema
+from ..schemas import CompletionSchema
+from ..schemas import ResponseSchema
+from ..schemas import AttachmentsSchema
 
 
 class Completions(Resource):
     method_decorators = [authenticate]
 
-    def post(self, hash_id=None):
-        json_payload = request.json
-
-        completion_schema = CompletionSchema()
-
+    def post(self, hash_id):
         try:
-            validated_completion_request = completion_schema.load(json_payload)
-        except ValidationError as error:
-            return jsonify(error.messages)
+            json_payload = request.json
 
-        personal_number = hash_to_personal_number(
-            hash_id=hash_id)
+            completion_schema = CompletionSchema()
+            validated_completion_payload = completion_schema.load(json_payload)
 
-        dummy_response = {
-            'ERRORCODE': '400',
-            'ERRORMESSAGE': 'TEST WORKS',
-            'STATUS': '400',
-            'IDENCLAIR': 'idenclear',
-            'ID': 'IDIDIDIDIDIDIDIDID'
-        }
+            validated_completion_payload['application_type'] = 'completion'
 
-        response_schema = ResponseSchema()
-        try:
-            validated_response = response_schema.load(dummy_response)
+            personal_number = hash_to_personal_number(hash_id=hash_id)
+
+            viva_application = VivaApplication(
+                my_pages=VivaMyPages(user=personal_number),
+                viva_attachments=VivaAttachments(user=personal_number),
+                application=validated_completion_payload)
+
+            submit_response = viva_application.submit()
+
+            response_schema = ResponseSchema()
+            validated_response = response_schema.load(submit_response)
+
             return validated_response
-        except ValidationError as error:
+
+        except (ValidationError, Fault) as error:
             return jsonify(error.messages)
