8e4f2487ef425324a2ce873d9e5e9b7c96117b1f
Dan Nilsson <dan.nilsson@helsingborg.se>
2021-02-26 16:47:59 +0100

    fix: 🐛 adapt to completion implementation
    remove obsoleted get

--- app/libs/classes/viva_attachments.py
+++ app/libs/classes/viva_attachments.py
@@ -1,35 +1,28 @@
-from zeep.exceptions import ValidationError
+from zeep.exceptions import Fault
+
 from . import Viva
 
 
 class VivaAttachments(Viva):
 
-    def __init__(self, my_pages, wsdl='VivaAttachment'):
+    def __init__(self, user, wsdl='VivaAttachment'):
         super(VivaAttachments, self).__init__()
 
-        self._my_pages = my_pages
         self._service = self._get_service(wsdl)
+        self._user = user
 
     def save(self, attachment=dict):
         if not isinstance(attachment, dict):
-            raise ValidationError('Invalid attachment')
+            raise Fault(message='Invalid attachment dict')
 
         viva_save_soap_response = self._service.SAVEDATA(
-            SUSER=self._my_pages.get_personal_number(),
+            SUSER=self._user,
             SIP='0.0.0.0',
             STEMP='5',
-            SKEY=attachment['attachment_id'],
+            SKEY=attachment['id'],
             SPARENTID='',
-            SFILENAME=attachment['file_name'],
+            SFILENAME=attachment['name'],
             SDATA=attachment['file_base64']
         )
 
         return self._helpers.serialize_object(viva_save_soap_response)
-
-    def get(self, attachment_id=str):
-        viva_get_soap_response = self._service.GETDATA(
-            SUSER=self._my_pages.get_personal_number(),
-            GKEY=attachment_id
-        )
-
-        return self._helpers.serialize_object(viva_get_soap_response)
