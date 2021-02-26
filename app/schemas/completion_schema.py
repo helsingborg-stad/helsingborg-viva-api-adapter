da71f13210d64fd5aa8a700c50116615dc7e6750
Dan Nilsson <dan.nilsson@helsingborg.se>
2021-02-26 16:45:07 +0100

    feat: 🎸 change answers for attachment

--- app/schemas/completion_schema.py
+++ app/schemas/completion_schema.py
@@ -1,13 +1,25 @@
-from marshmallow import Schema, fields
-from .answer_schema import AnswerSchema
+from marshmallow import validate, Schema, fields
+
+
+class AttachmentSchema(Schema):
+    id = fields.String(required=True)
+    name = fields.String(required=True)
+    category = fields.String(
+        required=True,
+        validate=validate.OneOf(['incomes', 'expenses', 'completion'])
+    )
+    file_base64 = fields.String(
+        data_key='fileBase64',
+        required=True
+    )
 
 
 class CompletionSchema(Schema):
-    workflow_id = fields.Str(
+    workflow_id = fields.String(
         data_key='workflowId',
         required=True
     )
-    answers = fields.List(
-        fields.Nested(AnswerSchema(), required=True),
+    attachments = fields.List(
+        fields.Nested(AttachmentSchema(), required=True),
         required=True
     )
