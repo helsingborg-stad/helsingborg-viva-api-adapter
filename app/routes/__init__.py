fbb2c583c6c972577542b5279e116e4b6fff8e2c
Dan Nilsson <dan.nilsson@helsingborg.se>
2021-02-26 16:43:16 +0100

    fix: 🐛 order issue

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,6 +3,6 @@ from .my_pages_workflows import MyPagesWorkflows
 from .applications import Applications
 from .attachments import Attachments
 from .attachment import Attachment
+from .completions import Completions
 from .test_cookie import TestCookie
 from .index import Index
-from .completions import Completions
