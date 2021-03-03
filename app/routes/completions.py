3c9aa6250c59af47eda2e42fb22754213c7122f9
Dan Nilsson <mail@dannilsson.se>
2021-03-03 22:58:30 +0100

    fix: 🐛 implement refactored VivaApplication class

--- app/routes/completions.py
+++ app/routes/completions.py
@@ -3,6 +3,7 @@ from flask_restful import Resource
 from zeep.exceptions import Fault
 from marshmallow import ValidationError
 
+from ..libs import DataClassApplication
 from ..libs import VivaMyPages
 from ..libs import VivaAttachments
 from ..libs import VivaApplication
@@ -20,17 +21,18 @@ class Completions(Resource):
         try:
             json_payload = request.json
 
+            personal_number = hash_to_personal_number(hash_id=hash_id)
+
             completion_schema = CompletionSchema()
             validated_completion_payload = completion_schema.load(json_payload)
-
-            validated_completion_payload['application_type'] = 'completion'
-
-            personal_number = hash_to_personal_number(hash_id=hash_id)
 
             viva_application = VivaApplication(
                 my_pages=VivaMyPages(user=personal_number),
                 viva_attachments=VivaAttachments(user=personal_number),
-                application=validated_completion_payload)
+                application=DataClassApplication(
+                    operation_type='completion',
+                    attachments=validated_completion_payload['attachments'],
+                    workflow_id=validated_completion_payload['workflow_id']))
 
             submit_response = viva_application.submit()
 
