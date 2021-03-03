9051eb86f658e7f046faaabf14601da8164e0393
Dan Nilsson <mail@dannilsson.se>
2021-03-03 22:57:55 +0100

    fix: 🐛 implement refactored VivaApplication class
    fix try catch block

--- app/routes/applications.py
+++ app/routes/applications.py
@@ -22,8 +22,8 @@ class Applications(Resource):
             personal_number = hash_to_personal_number(hash_id=hash_id)
 
             viva_application = VivaApplication(
-                application=DataClassApplication(operation_type='status'),
-                my_pages=VivaMyPages(user=personal_number))
+                my_pages=VivaMyPages(user=personal_number),
+                application=DataClassApplication(operation_type='status'))
 
             response = viva_application.submit()
 
@@ -36,26 +36,31 @@ class Applications(Resource):
             }, fault.code
 
     def post(self):
-        json_payload = request.json
-
-        application_schema = ApplicationSchema()
-
         try:
+            json_payload = request.json
+
+            application_schema = ApplicationSchema()
             validated_application = application_schema.load(json_payload)
-        except ValidationError as error:
-            return jsonify(error.messages)
 
-        personal_number = hash_to_personal_number(
-            hash_id=validated_application['hashid'])
+            personal_number = hash_to_personal_number(
+                hash_id=validated_application['hashid'])
 
-        viva_application = VivaApplication(
-            application=validated_application, my_pages=VivaMyPages(user=personal_number))
+            viva_application = VivaApplication(
+                my_pages=VivaMyPages(user=personal_number),
+                application=DataClassApplication(
+                    operation_type=validated_application['application_type'],
+                    workflow_id=validated_application['workflow_id'],
+                    answers=validated_application['answers'],
+                    raw_data=validated_application['raw_data']))
 
-        response = viva_application.submit()
+            response = viva_application.submit()
 
-        response_schema = ResponseSchema()
-        try:
+            response_schema = ResponseSchema()
             validated_response = response_schema.load(response)
+
             return validated_response
-        except ValidationError as error:
-            return jsonify(error.messages)
+
+        except Exception as error:
+            return {
+                'message': error.message,
+            }, 500
