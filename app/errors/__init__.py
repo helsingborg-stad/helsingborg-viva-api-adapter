218bd7ee1f4d761c76d52fe6a849de4c075d0193
Teddy Andersson <teddyandersson95@gmail.com>
2021-04-14 15:56:56 +0200

    feat(errors): first implementation of error classes to use in the custom handle_error function

---
+++ app/errors/__init__.py
@@ -0,0 +1,2 @@
+from .base_error import BaseError
+from .validation_error import ValidationError
---
+++ app/errors/validation_error.py
@@ -0,0 +1,8 @@
+from .base_error import BaseError
+
+
+class ValidationError(BaseError):
+    """ Should be raised in case of custom validation """
+
+    def __init__(self, message):
+        super().__init__(http_status_code=400, message=message)
