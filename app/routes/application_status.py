ea43587c290cc07d3decb47e7ae43a1cb0fdf1f5
Teddy Andersson <teddyandersson95@gmail.com>
2021-04-15 10:34:20 +0200

    refactor(routes): validation for hash_id decoding in route application_sattus
    The get method on the ApplicationStatus resource class now raise an validation if the hash_id can't
    be decoded. This error is catched by the flask_restful api in order to return an human readable
    error message with the status code 400 Bad Request

--- app/routes/application_status.py
+++ app/routes/application_status.py
@@ -2,23 +2,26 @@ from flask_restful import Resource
 from zeep.exceptions import Fault
 
 from ..libs import VivaApplicationStatus
-from ..libs import hash_to_personal_number
+from ..libs import decode_hash_id
 from ..errors import ValidationError
 
 
 class ApplicationStatus(Resource):
 
     def get(self, hash_id):
-        if not len(hash_id) == 32:
+        hash_id_tuple = decode_hash_id(hash_id=hash_id)
+
+        if len(hash_id_tuple) == 0:
             raise ValidationError(
                 message=f'The hashid {hash_id} in the url is not a valid hashid')
 
-        personal_number = hash_to_personal_number(hash_id=hash_id)
-        print(personal_number)
+        personal_number = hash_id_tuple[0]
+
+        if current_app.config['ENV'] in ['development', 'test']:
+            personal_number = to_test_personal_number(personal_number)
 
         viva_application_status = VivaApplicationStatus(
             personal_number=personal_number)
-        print(viva_application_status)
 
         response = viva_application_status.get()
 
