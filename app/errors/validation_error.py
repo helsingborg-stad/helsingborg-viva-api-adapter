c21a9e68ac158948aa2b89017a233a72ee080766
Teddy Andersson <teddyandersson95@gmail.com>
2021-04-15 16:56:13 +0200

    refactor(validation_error): removed descriptive comment due to name change of class

--- app/errors/validation_error.py
+++ app/errors/validation_error.py
@@ -2,7 +2,6 @@ from . import BaseError
 
 
 class CustomValidationError(BaseError):
-    """ Should be raised in case of custom validation """
 
     def __init__(self, message):
         super().__init__(http_status_code=400, message=message)
