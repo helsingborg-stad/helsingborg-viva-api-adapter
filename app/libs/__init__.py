3d8cd7500b19bf6916e7fc4131dae397f871fa85
Teddy Andersson <teddyandersson95@gmail.com>
2021-04-15 16:32:56 +0200

    fix(helpers): removed trailing coma after import

--- app/libs/__init__.py
+++ app/libs/__init__.py
@@ -6,7 +6,7 @@ from .classes import VivaApplication
 from .classes import VivaApplicationStatus
 from .classes import VivaAttachments
 
-from .personal_number_helper import hashids_instance,
+from .personal_number_helper import hashids_instance
 from .personal_number_helper import hash_to_personal_number
 from .personal_number_helper import decode_hash_id
 
