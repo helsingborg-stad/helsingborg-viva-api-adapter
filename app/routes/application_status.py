9683612adaa3b60bb0a25faac66fb9aa72f1791b
Dan Nilsson <dan.nilsson@helsingborg.se>
2021-04-27 15:39:46 +0200

    fix: 🐛 reimplement missing hashid helper
    to make the code run

--- app/routes/application_status.py
+++ app/routes/application_status.py
@@ -3,23 +3,14 @@ from flask import current_app
 from zeep.exceptions import Fault
 
 from ..libs import VivaApplicationStatus
-from ..libs import decode_hash_id
+from ..libs import hash_to_personal_number
 from ..errors import CustomValidationError
 
 
 class ApplicationStatus(Resource):
 
     def get(self, hash_id):
-        decoded_hash_id = decode_hash_id(hash_id=hash_id)
-
-        if len(decoded_hash_id) == 0:
-            raise CustomValidationError(
-                message=f'The server cannot or will not process the request due to something that is perceived to be a client error (e.g., malformed request syntax, invalid request message framing, or deceptive request routing).')
-
-        personal_number = decoded_hash_id[0]
-
-        if current_app.config['ENV'] in ['development', 'test']:
-            personal_number = to_test_personal_number(personal_number)
+        personal_number = hash_to_personal_number(hash_id=hash_id)
 
         viva_application_status = VivaApplicationStatus(
             personal_number=personal_number)
