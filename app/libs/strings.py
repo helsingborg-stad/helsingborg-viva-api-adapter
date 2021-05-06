3d319f33ae72ef5a6b242838d730c6201175481d
Teddy Andersson <teddyandersson95@gmail.com>
2021-05-06 14:19:46 +0200

    refactor(strings): simplified function trim_last_character
    Making the trim operation directly on the string variable instead of first turning the string to a
    list and then call join.

--- app/libs/strings.py
+++ app/libs/strings.py
@@ -1,2 +1,5 @@
-def trim_last_character(str):
-    return "".join(list(str)[:-1])
+def trim_last_character(string: str = None):
+    if not isinstance(string, str):
+        raise TypeError(f'expected {string} to be of type string')
+
+    return string[:-1]
