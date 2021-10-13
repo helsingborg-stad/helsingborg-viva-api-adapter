2a58b4775ac515a241c1b0b107073873537c2645
Dan Nilsson <mail@dannilsson.se>
2021-10-13 16:24:03 +0200

    hotfix: remove to_test_personal_number function
    Obsoleted. All test users in Viva test have production like formation with '-' ie. YYYYMMDD-XXXX
    No more 'T'

--- app/libs/__init__.py
+++ app/libs/__init__.py
@@ -9,7 +9,6 @@ from .classes import VivaAttachments
 from .personal_number_helper import hashids_instance
 from .personal_number_helper import hash_to_personal_number
 from .personal_number_helper import decode_hash_id
-from .personal_number_helper import to_test_personal_number
 
 from .datetime_helper import milliseconds_to_date_string
 from .authenticate_helper import authenticate
--- app/libs/personal_number_helper.py
+++ app/libs/personal_number_helper.py
@@ -24,33 +24,17 @@ def hash_to_personal_number(hash_id=None):
 
     personal_number = str(decoded_hash_id[0])
 
-    if current_app.config['ENV'] in ['development', 'test']:
-        test_personal_number = to_test_personal_number(personal_number)
-        return test_personal_number
-
     viva_personal_number = to_viva_formatted_personal_number(personal_number)
     return viva_personal_number
 
 
 def split_personal_number(personal_number=str):
+    # YYYYMMDDXXXX parts becomes [YYYYMMDD, XXXX]
     regex = re.compile('([0-9]{8})([0-9]{4})')
     parts = regex.match(personal_number).groups()
     return parts
 
 
 def to_viva_formatted_personal_number(personal_number=str):
-    personal_number_split_list = split_personal_number(personal_number)
-    return '-'.join(personal_number_split_list)
-
-
-def to_test_personal_number(personal_number=str):
-    if personal_number in ('195809262743', '199803312397', '196709132887', '198602102397',
-                           '198602272380', '198603072383', '198603072391', '199803092387'):
-        # VIVA, navet test format
-        # yyyymmdd-nnnn
-        return to_viva_formatted_personal_number(personal_number)
-
-    # Default VIVA test format
-    # yyyymmddTnnnn
-    personal_number_split_list = split_personal_number(personal_number)
-    return 'T'.join(personal_number_split_list)
+    personal_number_split = split_personal_number(personal_number)
+    return '-'.join(personal_number_split)
