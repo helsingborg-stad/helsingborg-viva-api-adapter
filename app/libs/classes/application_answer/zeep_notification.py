1673463dcbc5fba34b4e3e720ed7cae04e7baa3f
Dan Nilsson <dan.nilsson@helsingborg.se>
2021-12-02 20:12:26 +0100

    wip: add new answers to zeep class

---
+++ app/libs/classes/application_answer/zeep_notification.py
@@ -0,0 +1,63 @@
+from . import ApplicationAnswerCollection
+from ...classes.viva_my_pages import VivaMyPages
+
+# Hämta svar som har tag = notifcation (notifcation,coapplicant)
+# Personnummer för samtliga sökande
+# Matcha mobilnummer med personnummer
+
+
+class ZeepNotification(dict):
+
+    ADRESS_TYPE_SMS = 'sms'
+    EMPTY_NOTIFCATION = {
+        'id': '',
+        'adress': '',
+        'adresstype': ADRESS_TYPE_SMS,
+    }
+
+    def __init__(self, my_pages: VivaMyPages, application_answer_collection: ApplicationAnswerCollection = []):
+        super().__init__(self)
+        self.application_answer_collection = application_answer_collection
+        self._my_pages = my_pages
+        self._create()
+
+    def _create(self):
+        [{
+            field: {
+                tags: ['noti', 'coapplicant']
+            },
+            value: '0148184184'
+        },
+            {
+            field: {
+                tags: ['noti', 'applicant']
+            },
+            value: '0148184184'
+        }]
+        # get answers with tags ['notification', 'phonenumber']
+        notification_answers = self.get_notification_answers()
+
+        # get persons with role and personalnumber
+        notification_list = self.get_notification_list(
+            answers=notification_answers)
+
+        return notification_list
+
+    def _get_notification_list(self, answers):
+        if (answers):
+            for answer in answers:
+
+                for person in persons:
+                    if answer.has_tag(person.role):
+                        notification_list.append({
+                            id: person.personalNumber,
+                            addresstype: 'sms',
+                            address: answer.value
+                        })
+
+    def _get_notification_answers(self):
+        tags = ['notification', 'phonenumber']
+        return self.application_answer_collection.filter_by_tags(tags=tags)
+
+    def _get_persons(self):
+        persons = self._my_pages.person_cases['vivadata']['vivacases']['vivacase']
