39d8b1890665431db4638116e8d672e35a4ab40d
Dan Nilsson <mail@dannilsson.se>
2021-12-05 23:51:15 +0100

    wip: add pseudo code

--- app/libs/classes/application_answer/zeep_notification.py
+++ app/libs/classes/application_answer/zeep_notification.py
@@ -15,30 +15,30 @@ class ZeepNotification(dict):
         'adresstype': ADRESS_TYPE_SMS,
     }
 
-    def __init__(self, my_pages: VivaMyPages, application_answer_collection: ApplicationAnswerCollection = []):
+    def __init__(self, my_pages: VivaMyPages, application_answer_collection: ApplicationAnswerCollection = {}):
         super().__init__(self)
         self.application_answer_collection = application_answer_collection
         self._my_pages = my_pages
         self._create()
 
     def _create(self):
-        [{
-            field: {
-                tags: ['noti', 'coapplicant']
-            },
-            value: '0148184184'
-        },
-            {
-            field: {
-                tags: ['noti', 'applicant']
-            },
-            value: '0148184184'
-        }]
-        # get answers with tags ['notification', 'phonenumber']
-        notification_answers = self.get_notification_answers()
+        # [{
+        #     field: {
+        #         tags: ['noti', 'coapplicant']
+        #     },
+        #     value: '0148184184'
+        # },
+        #     {
+        #     field: {
+        #         tags: ['noti', 'applicant']
+        #     },
+        #     value: '0148184184'
+        # }]
+        # get answers with tags ['notification', 'sms']
+        notification_answers = self._get_notification_answers()
 
         # get persons with role and personalnumber
-        notification_list = self.get_notification_list(
+        notification_list = self._get_notification_list(
             answers=notification_answers)
 
         return notification_list
@@ -56,7 +56,7 @@ class ZeepNotification(dict):
                         })
 
     def _get_notification_answers(self):
-        tags = ['notification', 'phonenumber']
+        tags = ['notification', 'sms']
         return self.application_answer_collection.filter_by_tags(tags=tags)
 
     def _get_persons(self):
