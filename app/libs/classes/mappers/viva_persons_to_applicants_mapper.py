0c56a76530fbe328a70c76514924f3550bdea19b
Teddy Andersson <teddyandersson95@gmail.com>
2021-12-09 13:59:22 +0100

    feat(viva_persons_mapper): a mapper class that maps viva persons and creates applicants
    This was introduced in order to get applicants with a role assigned to them. The role can either be
    applicant or coapplicant. Test has also been added to this mapper class

---
+++ app/libs/classes/mappers/viva_persons_to_applicants_mapper.py
@@ -0,0 +1,24 @@
+class VivaPersonsToApplicantsMapper():
+    def __init__(self, applicant, coapplicant):
+        self._applicant = applicant
+        self._coapplicant = coapplicant
+
+    def get_applicants(self):
+        return list(a for a in [self._get_applicant(), self._get_coapplicant()] if a)
+
+    def _get_applicant(self):
+        if self._applicant:
+            return {
+                "role": "applicant",
+                "personalnumber": self._applicant["pnumber"]
+            }
+        return None
+
+    def _get_coapplicant(self):
+        if self._coapplicant:
+            return {
+                "role": "coapplicant",
+                "personalnumber": self._coapplicant["pnumber"]
+            }
+
+        return None
