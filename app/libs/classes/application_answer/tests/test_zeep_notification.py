ade8c2964e820289baf03089ecc23fafab49d965
Teddy Andersson <teddyandersson95@gmail.com>
2021-12-09 14:04:48 +0100

    refactor(test_zeep_notification): using assert directly to compare output

--- app/libs/classes/application_answer/tests/test_zeep_notification.py
+++ app/libs/classes/application_answer/tests/test_zeep_notification.py
@@ -4,18 +4,12 @@ from .. import ApplicationAnswerCollection
 from .. import ZeepNotification
 
 
-def anscoll(*args):
+def answer_collection(*args):
     return ApplicationAnswerCollection(*args)
 
 
-def ans(value, tags):
+def answer(value, tags):
     return ApplicationAnswer(value=value, tags=tags)
-
-
-def assert_notification(actual, expected):
-    assert expected["id"] == actual["id"]
-    assert expected["adresstype"] == actual["adresstype"]
-    assert expected["adress"] == actual["adress"]
 
 
 def applicants():
@@ -32,58 +26,58 @@ def applicants():
 
 
 def test_applicant_happy_path():
-    notification = ZeepNotification(applicants(), application_answer_collection=anscoll(
-        ans("yes", ["applicant", "nofification", "sms"]),
-        ans("0708134506", ["applicant", "phonenumber"]),
+    notification = ZeepNotification(applicants(), application_answer_collection=answer_collection(
+        answer("yes", ["applicant", "nofification", "sms"]),
+        answer("0708134506", ["applicant", "phonenumber"]),
     ))
-    sms_notification = notification.get_sms("applicant")
-    assert_notification(sms_notification, {
+
+    assert notification.get_sms("applicant") == {
         "id": '19900102034444',
         "adresstype": 'sms',
         "adress": '0708134506'
-    })
+    }
 
 
 def test_coapplicant_happy_path():
-    notification = ZeepNotification(applicants(), application_answer_collection=anscoll(
-        ans("yes", ["coapplicant", "nofification", "sms"]),
-        ans("0708134509", ["coapplicant", "phonenumber"]),
+    notification = ZeepNotification(applicants(), application_answer_collection=answer_collection(
+        answer("yes", ["coapplicant", "nofification", "sms"]),
+        answer("0708134509", ["coapplicant", "phonenumber"]),
     ))
-    sms_notification = notification.get_sms("coapplicant")
-    assert_notification(sms_notification, {
+
+    assert notification.get_sms("coapplicant") == {
         "id": '19900102035555',
         "adresstype": 'sms',
         "adress": '0708134509'
-    })
+    }
 
 
 def test_no_applicants():
-    notification = ZeepNotification([], application_answer_collection=anscoll(
-        ans("yes", ["applicant", "nofification", "sms"]),
-        ans("0708134506", ["applicant", "phonenumber"]),
-        ans("yes", ["coapplicant", "nofification", "sms"]),
-        ans("0708134508", ["coapplicant", "phonenumber"]),
+    notification = ZeepNotification([], application_answer_collection=answer_collection(
+        answer("yes", ["applicant", "nofification", "sms"]),
+        answer("0708134506", ["applicant", "phonenumber"]),
+        answer("yes", ["coapplicant", "nofification", "sms"]),
+        answer("0708134508", ["coapplicant", "phonenumber"]),
     ))
     assert notification.get_sms("applicant") == None
     assert notification.get_sms("coapplicant") == None
 
 
 def test_notification_not_wanted_by_user():
-    notification = ZeepNotification(applicants(), application_answer_collection=anscoll(
-        ans("", ["applicant", "nofification", "sms"]),
-        ans("0708134506", ["applicant", "phonenumber"]),
-        ans("", ["coapplicant", "nofification", "sms"]),
-        ans("0708134508", ["coapplicant", "phonenumber"]),
+    notification = ZeepNotification(applicants(), application_answer_collection=answer_collection(
+        answer("", ["applicant", "nofification", "sms"]),
+        answer("0708134506", ["applicant", "phonenumber"]),
+        answer("", ["coapplicant", "nofification", "sms"]),
+        answer("0708134508", ["coapplicant", "phonenumber"]),
     ))
     assert notification.get_sms("applicant") == None
     assert notification.get_sms("coapplicant") == None
 
 
 def test_no_or_empty_phonenumber_submitted():
-    notification = ZeepNotification(applicants(), application_answer_collection=anscoll(
-        ans("yes", ["applicant", "nofification", "sms"]),
-        ans("", ["applicant", "phonenumber"]),
-        ans("yes", ["coapplicant", "nofification", "sms"]),
+    notification = ZeepNotification(applicants(), application_answer_collection=answer_collection(
+        answer("yes", ["applicant", "nofification", "sms"]),
+        answer("", ["applicant", "phonenumber"]),
+        answer("yes", ["coapplicant", "nofification", "sms"]),
     ))
     assert notification.get_sms("applicant") == None
     assert notification.get_sms("coapplicant") == None
