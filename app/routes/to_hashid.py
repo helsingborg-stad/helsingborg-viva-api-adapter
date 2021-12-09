e700156c2093287ffb125641a9da630932be34d7
Teddy Andersson <teddyandersson95@gmail.com>
2021-12-09 13:55:27 +0100

    fix(hash_ids): reference to hashids_instance have been replaced
    Instead of importing a global instance directly we now use a function to cache the hash_ids instance
    thorughout the code base. This is due to that using a global instance in a helper file is considered
    bad practice

--- app/routes/to_hashid.py
+++ app/routes/to_hashid.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from ..libs import hashids_instance
+from ..libs import get_hash_ids
 from ..libs import authenticate
 
 
@@ -8,7 +8,7 @@ class ToHashId(Resource):
 
     def get(self, personal_number: int):
 
-        hashid = hashids_instance.encode(personal_number)
+        hashid = get_hash_ids().encode(personal_number)
 
         response = {
             'hashid': hashid
