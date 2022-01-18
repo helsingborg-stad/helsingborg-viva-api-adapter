5b777807dccf4132c930a6d94c297ae967f872d4
Dan Nilsson <mail@dannilsson.se>
2022-01-18 18:57:43 +0100

    fix: 🐛 notify object keys to uppercase
    Viva expect keys in uppercase

--- app/libs/classes/application_answer/tests/test_zeep_notification.py
+++ app/libs/classes/application_answer/tests/test_zeep_notification.py
@@ -27,57 +27,57 @@ def applicants():
 
 def test_sms_list_happy_path():
     notification = ZeepNotification(applicants(), application_answer_collection=answer_collection(
-        answer('yes', ['applicant', 'nofification', 'sms']),
+        answer(True, ['applicant', 'nofification', 'sms']),
         answer('0708134506', ['applicant', 'phonenumber']),
-        answer('yes', ['coapplicant', 'nofification', 'sms']),
+        answer(True, ['coapplicant', 'nofification', 'sms']),
         answer('0708134509', ['coapplicant', 'phonenumber']),
     ))
 
     assert notification.get_sms_list(applicant_tags=['applicant', 'coapplicant']) == [
         {
-            'id': '19900102034444',
-            'adresstype': 'sms',
-            'adress': '0708134506'
+            'ID': '19900102034444',
+            'ADDRESS': '0708134506',
+            'ADDRESSTYPE': 'sms',
         },
         {
-            'id': '19900102035555',
-            'adresstype': 'sms',
-            'adress': '0708134509'
+            'ID': '19900102035555',
+            'ADDRESS': '0708134509',
+            'ADDRESSTYPE': 'sms',
         }
     ]
 
 
 def test_applicant_happy_path():
     notification = ZeepNotification(applicants(), application_answer_collection=answer_collection(
-        answer('yes', ['applicant', 'nofification', 'sms']),
+        answer(True, ['applicant', 'nofification', 'sms']),
         answer('0708134506', ['applicant', 'phonenumber']),
     ))
 
     assert notification.get_sms('applicant') == {
-        'id': '19900102034444',
-        'adresstype': 'sms',
-        'adress': '0708134506'
+        'ID': '19900102034444',
+        'ADDRESS': '0708134506',
+        'ADDRESSTYPE': 'sms',
     }
 
 
 def test_coapplicant_happy_path():
     notification = ZeepNotification(applicants(), application_answer_collection=answer_collection(
-        answer('yes', ['coapplicant', 'nofification', 'sms']),
+        answer(True, ['coapplicant', 'nofification', 'sms']),
         answer('0708134509', ['coapplicant', 'phonenumber']),
     ))
 
     assert notification.get_sms('coapplicant') == {
-        'id': '19900102035555',
-        'adresstype': 'sms',
-        'adress': '0708134509'
+        'ID': '19900102035555',
+        'ADDRESS': '0708134509',
+        'ADDRESSTYPE': 'sms',
     }
 
 
 def test_no_applicants():
     notification = ZeepNotification([], application_answer_collection=answer_collection(
-        answer('yes', ['applicant', 'nofification', 'sms']),
+        answer(True, ['applicant', 'nofification', 'sms']),
         answer('0708134506', ['applicant', 'phonenumber']),
-        answer('yes', ['coapplicant', 'nofification', 'sms']),
+        answer(True, ['coapplicant', 'nofification', 'sms']),
         answer('0708134508', ['coapplicant', 'phonenumber']),
     ))
     assert notification.get_sms('applicant') is None
@@ -97,9 +97,9 @@ def test_notification_not_wanted_by_user():
 
 def test_no_or_empty_phonenumber_submitted():
     notification = ZeepNotification(applicants(), application_answer_collection=answer_collection(
-        answer('yes', ['applicant', 'nofification', 'sms']),
+        answer(True, ['applicant', 'nofification', 'sms']),
         answer('', ['applicant', 'phonenumber']),
-        answer('yes', ['coapplicant', 'nofification', 'sms']),
+        answer(True, ['coapplicant', 'nofification', 'sms']),
     ))
     assert notification.get_sms('applicant') is None
     assert notification.get_sms('coapplicant') is None
@@ -107,29 +107,29 @@ def test_no_or_empty_phonenumber_submitted():
 
 def test_sms_list_applicant_only():
     notification = ZeepNotification(applicants(), application_answer_collection=answer_collection(
-        answer('yes', ['applicant', 'nofification', 'sms']),
+        answer(True, ['applicant', 'nofification', 'sms']),
         answer('0708134506', ['applicant', 'phonenumber']),
     ))
 
     assert notification.get_sms_list(applicant_tags=['applicant']) == [
         {
-            'id': '19900102034444',
-            'adresstype': 'sms',
-            'adress': '0708134506'
+            'ID': '19900102034444',
+            'ADDRESS': '0708134506',
+            'ADDRESSTYPE': 'sms',
         }
     ]
 
 
 def test_sms_list_coapplicant_only():
     notification = ZeepNotification(applicants(), application_answer_collection=answer_collection(
-        answer('yes', ['coapplicant', 'nofification', 'sms']),
+        answer(True, ['coapplicant', 'nofification', 'sms']),
         answer('0708134509', ['coapplicant', 'phonenumber']),
     ))
 
     assert notification.get_sms_list(applicant_tags=['coapplicant']) == [
         {
-            'id': '19900102035555',
-            'adresstype': 'sms',
-            'adress': '0708134509'
+            'ID': '19900102035555',
+            'ADDRESS': '0708134509',
+            'ADDRESSTYPE': 'sms',
         }
     ]
