ae9571361dfb518017c82ba0bb9aa6fea5b54709
Dan Nilsson <mail@dannilsson.se>
2022-06-04 21:38:51 +0200

    feat: 🎸 (new) add attachment to application viva zeep

--- app/libs/classes/viva_application.py
+++ app/libs/classes/viva_application.py
@@ -1,5 +1,4 @@
-from app.libs.enum import ApplicationType
-
+from app.routes import attachment
 from . import DataClassApplication
 from . import Viva
 from . import VivaMyPages
@@ -41,10 +40,10 @@ class VivaApplication(Viva):
         self._viva_attachments = viva_attachments
         self._service = self._get_service(wsdl)
 
-        self._viva_soap_operation_types: ApplicationType = {
-            ApplicationType.NEW: self._new_application,
-            ApplicationType.RECURRING: self._new_re_application,
-            ApplicationType.COMPLETION: self._new_completion,
+        self._viva_soap_operation_types = {
+            'new': self._new_application,
+            'recurrent': self._new_re_application,
+            'completion': self._new_completion,
         }
 
         self._operation_type = application.operation_type
@@ -71,7 +70,7 @@ class VivaApplication(Viva):
 
         return answer_collection
 
-    def _create_recurring_application(self):
+    def _get_application(self):
         initial_application = {
             'OTHER': '',
             'RAWDATA': self._raw_data,
@@ -97,17 +96,17 @@ class VivaApplication(Viva):
         if not client:
             raise ValueError(f'Client can not be {client}. Verify your tags!')
 
-        attachment_list = self._get_zeep_attachment_list()
+        attachment_list = self._get_completion_attachments()
         new_application = self._get_zeep_application_dict()
 
         return {**initial_new_application, **client, **new_application, **attachment_list}
 
-    def _save_attachments(self):
+    def _save_completion_attachments(self):
         for attachment in self._attachments:
             self._viva_attachments.save(attachment=attachment)
         return True
 
-    def _get_zeep_attachment_list(self):
+    def _get_completion_attachments(self):
         attachment_category_type = {
             'incomes': 'Inkomster',
             'expenses': 'Utgifter',
@@ -152,7 +151,7 @@ class VivaApplication(Viva):
         return zeep_dict
 
     def _new_application(self):
-        self._save_attachments()
+        self._save_completion_attachments()
         new_application = self._create_new_application()
 
         response = self._service.NEWAPPLICATION(
@@ -185,7 +184,7 @@ class VivaApplication(Viva):
             # Period som ansökan avser
             PERIOD=self._my_pages.get_period(),
 
-            REAPPLICATION=self._create_recurring_application(),
+            REAPPLICATION=self._get_application(),
 
             NOTIFYINFOS={
                 'NOTIFYINFO': self._get_zeep_notfication_list()
@@ -195,11 +194,11 @@ class VivaApplication(Viva):
         return self._helpers.serialize_object(response)
 
     def _new_completion(self):
-        self._save_attachments()
+        self._save_completion_attachments()
 
         personal_number = self._my_pages.get_personal_number()
         case_ssi = self._my_pages.get_casessi()
-        completion = self._get_zeep_attachment_list()
+        completion = self._get_completion_attachments()
 
         completion_response = self._service.NEWCOMPLETION(
             KEY='',
