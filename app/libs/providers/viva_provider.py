015f19531ed72e81f5e8f7b044e1badff93decb0
MrSpex <mail@dannilsson.se>
2023-03-28 15:04:39 +0200

    refactor: implement abstract viva provider
    add abstract create client method

--- app/libs/providers/viva_provider.py
+++ app/libs/providers/viva_provider.py
@@ -1,15 +1,21 @@
+from abc import abstractmethod
 from app.libs.providers.ekb_abc_provider import EkbABCProvider
 from app.libs.data_domain.ekb_status import EkbStatus
 from app.libs.data_domain.ekb_mypages import EkbMyPages
 from app.libs.classes.viva_application_status import VivaApplicationStatus
 from app.libs.classes.viva_my_pages import VivaMyPages
+from app.libs.classes.viva import Viva
 
 
-class VivaProvider(EkbABCProvider):
+class AbstractVivaProvider(EkbABCProvider):
+
+    @abstractmethod
+    def create_client(self):
+        pass
 
     def get_status(self, id: str) -> EkbStatus:
         viva_application_status = VivaApplicationStatus(
-            personal_number=id)
+            personal_number=id, client=self.create_client())
 
         status: EkbStatus = viva_application_status.get()  # type: ignore
 
@@ -19,3 +25,10 @@ class VivaProvider(EkbABCProvider):
         person: EkbMyPages = VivaMyPages(user=id).person  # type: ignore
 
         return person
+
+
+class VivaProvider(AbstractVivaProvider):
+
+    def create_client(self):
+        viva = Viva()
+        return viva._get_service(wsdl_name='VivaApplication')
