7a8fadc7ee94d3a11e63bac6f0e3c69a5afb31df
MrSpex <mail@dannilsson.se>
2023-04-14 21:11:47 +0200

    feat: add abc get user method

--- app/libs/providers/ekb_abc_provider.py
+++ app/libs/providers/ekb_abc_provider.py
@@ -2,6 +2,7 @@ from typing import List
 from abc import ABC, abstractmethod
 from app.libs.data_domain.ekb_status import EkbStatus
 from app.libs.data_domain.ekb_mypages import EkbMyPages
+from app.libs.data_domain.ekb_user import EkbUser
 
 
 class EkbABCProvider(ABC):
@@ -12,4 +13,8 @@ class EkbABCProvider(ABC):
 
     @abstractmethod
     def get_mypages(self, id: str) -> EkbMyPages:
+        pass
+
+    @abstractmethod
+    def get_user(self, id: str) -> EkbUser:
         pass
