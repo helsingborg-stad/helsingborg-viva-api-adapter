b4aabfd17c60c3f8796f057f8e65e9206f8cff8d
MrSpex <mail@dannilsson.se>
2023-04-18 11:01:58 +0200

    refactor: auth decorator

--- app/libs/authenticate_helper.py
+++ app/libs/authenticate_helper.py
@@ -2,56 +2,44 @@ import jwt
 from flask import request, current_app
 from flask_restful import wraps
 from werkzeug.exceptions import Unauthorized
-from werkzeug.exceptions import BadRequest
 
 
-def validate_token() -> bool:
+def validate_token(token, public_key) -> bool:
     """
     JSON Web Tokens with Public Key Signatures
     https://blog.miguelgrinberg.com/post/json-web-tokens-with-public-key-signatures
     """
     try:
-        if 'X-API-Key' not in request.headers:
-            raise BadRequest(description='Header X-API-Key missing!')
-
-        token = request.headers['X-Api-Key']
-
-        if current_app.config['ENV'] in ('development', 'test'):
-            public_key = open('jwtRS256_dev.key.pub').read()
-        else:
-            public_key = open('jwtRS256.key.pub').read()
-
         jwt.decode(token, public_key, algorithms=['RS256'])
-
         return True
-
     except Exception as error:
         current_app.logger.debug(msg=error, exc_info=True)
         return False
 
 
 def authenticate(f):
-    """
-    Decorator function to validate token on route resources
 
-    Example:
-    class Applications(Resource):
-        method_decorators = [authenticate]
-        ...
-        ...
-    """
-    @ wraps(f)
+    @wraps(f)
     def wrapper(*args, **kwargs):
-        if current_app.testing:
-            return True
+        try:
+            is_testing = current_app.testing
+            is_attribute_authenticate = getattr(f, 'authenticate', True)
 
-        if not getattr(f, 'authenticate', True):
+            if not is_attribute_authenticate or is_testing:
+                return f(*args, **kwargs)
+
+            public_key_file = current_app.config['PUBLIC_KEY_FILE']
+            public_key = open(public_key_file).read()
+
+            if not validate_token(token=request.headers['X-Api-Key'],
+                                  public_key=public_key):
+                raise Unauthorized(
+                    description='The server could not verify that you are authorized to access the URL requested.')
+
             return f(*args, **kwargs)
 
-        if not validate_token():
-            raise Unauthorized(
-                description='The server could not verify that you are authorized to access the URL requested.')
-
-        return f(*args, **kwargs)
+        except Exception as error:
+            current_app.logger.debug(msg=error, exc_info=True)
+            raise error
 
     return wrapper
