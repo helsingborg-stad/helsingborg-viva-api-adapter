ed86efce12db261067ff8c4c4e3a06878e2dc948
MrSpex <mail@dannilsson.se>
2023-04-20 11:21:23 +0200

    refactor: extract skip auth to helper func

--- app/libs/authenticate_helper.py
+++ app/libs/authenticate_helper.py
@@ -4,6 +4,10 @@ from flask_restful import wraps
 from werkzeug.exceptions import Unauthorized
 
 
+def skip_authentication() -> bool:
+    return current_app.testing or current_app.debug
+
+
 def validate_token(token, public_key) -> bool:
     """
     JSON Web Tokens with Public Key Signatures
@@ -22,17 +26,16 @@ def authenticate(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
         try:
-            is_testing = current_app.testing
-            is_attribute_authenticate = getattr(f, 'authenticate', True)
+            is_set_attribute_authenticate = getattr(f, 'authenticate', True)
+            will_skip_auth = is_set_attribute_authenticate and skip_authentication()
 
-            if not is_attribute_authenticate or is_testing:
+            if will_skip_auth:
                 return f(*args, **kwargs)
 
-            public_key_file = current_app.config['PUBLIC_KEY_FILE']
-            public_key = open(public_key_file).read()
+            public_key = open(current_app.config['PUBLIC_KEY_FILE']).read()
+            token = request.headers.get('X-Api-Key')
 
-            if not validate_token(token=request.headers['X-Api-Key'],
-                                  public_key=public_key):
+            if not validate_token(token=token, public_key=public_key):
                 raise Unauthorized(
                     description='The server could not verify that you are authorized to access the URL requested.')
 
