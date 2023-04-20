60e6d70ed4859961df41cd2203a249206a4aa452
MrSpex <mail@dannilsson.se>
2023-04-20 11:18:14 +0200

    refator: implement abstract session class
    clean up dead code

--- app/libs/classes/viva.py
+++ app/libs/classes/viva.py
@@ -1,26 +1,15 @@
-import zeep.helpers as zeep_helpers
-from flask import current_app
 from zeep.client import Client
-from zeep.settings import Settings
 
-from app.libs.classes.session import Session
+from app.libs.classes.session import AbstractSession
 
 
 class Viva():
 
-    def __init__(self,
-                 settings=Settings,
-                 session=Session,
-                 current_app=current_app,
-                 zeep_helpers=zeep_helpers
-                 ):
-        self._settings = settings(
-            strict=False, xml_huge_tree=True)  # type: ignore
-        self._config = current_app.config['VIVA']
-        self._helpers = zeep_helpers
-        self._session = session()
+    def __init__(self, config: dict, session: AbstractSession):
+        self._config = config
+        self._session = session
 
-    def _get_service(self, wsdl_name):
+    def get_service(self, wsdl_name):
         transport = self._session.get_transport()
         wsdl_url = self._config['wsdl_url'] + '/' + wsdl_name + '?WSDL'
         client = Client(wsdl=wsdl_url, transport=transport)
