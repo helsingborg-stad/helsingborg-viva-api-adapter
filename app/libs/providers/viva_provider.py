d0c540d5014e6a9bd753f5f0d59a43a3cb6b334e
MrSpex <mail@dannilsson.se>
2023-04-20 11:20:17 +0200

    refactor: impl. exctracted session

--- app/libs/providers/viva_provider.py
+++ app/libs/providers/viva_provider.py
@@ -1,9 +1,15 @@
+from flask import current_app
+
 from app.libs.providers.viva_abc_provider import AbstractVivaProvider
 from app.libs.classes.viva import Viva
+from app.libs.classes.session import Session
 
 
 class VivaProvider(AbstractVivaProvider):
 
     def create_client(self, wsdl_name: str):
-        viva = Viva()
-        return viva._get_service(wsdl_name=wsdl_name)
+        config = current_app.config['VIVA']
+        session = Session(config=config)
+
+        viva = Viva(config=config, session=session)
+        return viva.get_service(wsdl_name=wsdl_name)
