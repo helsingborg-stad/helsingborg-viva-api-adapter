cf474c419ed013a9ef48a7212e22310930d08847
MrSpex <mail@dannilsson.se>
2023-04-20 08:34:57 +0200

    refactor: add env to create app

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
-app = create_app()
+env = os.environ.get('ENV', 'development')
+app = create_app(env=env)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
