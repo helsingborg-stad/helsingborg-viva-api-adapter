b81efb67c65cca67e2c96f4ee3e8d55fb4014ad1
MrSpex <mail@dannilsson.se>
2023-04-20 22:06:30 +0200

    feat: add provider class args

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,12 @@
 import os
+
 from app import create_app
+from app.libs.providers.viva_provider import VivaProvider
 
 env = os.environ.get('ENV', 'development')
-app = create_app(env=env)
+
+provider = VivaProvider()
+app = create_app(provider=provider, env=env)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
