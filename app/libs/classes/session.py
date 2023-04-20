bc667639d061b2b757a8992bd0928c99e69c0def
MrSpex <mail@dannilsson.se>
2023-04-20 11:17:46 +0200

    refactor: add abstact session class

--- app/libs/classes/session.py
+++ app/libs/classes/session.py
@@ -1,4 +1,5 @@
 import requests
+from abc import abstractmethod, ABC
 from flask import current_app
 from zeep.transports import Transport
 from zeep.cache import SqliteCache
@@ -6,25 +7,32 @@ from zeep.cache import SqliteCache
 from app.cache import cache
 
 
-class Session():
+class AbstractSession(ABC):
+
+    @abstractmethod
+    def __init__(self, config: dict) -> None:
+        self._config = config
+
+    @abstractmethod
+    def get_transport(self):
+        pass
+
+
+class Session(AbstractSession):
 
     _cookie = None
 
-    def __init__(self, transport=Transport, requests=requests, current_app=current_app):
-        self._config = current_app.config
-        self._requests = requests
-        self._transport = transport
-        self._sqlite_cache = SqliteCache('/tmp/viva_sqlite.db')
+    def __init__(self, config: dict):
+        self._config = config
 
     def get_transport(self):
         self._cookie = self._get_cookie()
 
-        cookie_jar = self._requests.cookies.RequestsCookieJar()
-        cookie_jar.set(self._config['COOKIE_AUTH_NAME'], self._cookie)
+        session = requests.Session()
+        session.cookies.set(self._config['COOKIE_AUTH_NAME'], self._cookie)
 
-        session = self._requests.Session()
-        session.cookies = cookie_jar
-        transport = self._transport(session=session, cache=self._sqlite_cache)
+        transport = Transport(
+            session=session, cache=SqliteCache('/tmp/viva_sqlite.db'))
 
         return transport
 
@@ -33,7 +41,7 @@ class Session():
         current_app.logger.debug(msg='COOKIE')
         login_conf = self._config['VIVA']['login']
 
-        response = self._requests.post(
+        response = requests.post(
             login_conf['url'],
             data={
                 'username': login_conf['username'],
