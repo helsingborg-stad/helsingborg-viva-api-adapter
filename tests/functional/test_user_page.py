04d6bf7a194a7ffa540a7587a70446a79b47f819
MrSpex <mail@dannilsson.se>
2023-04-21 16:24:08 +0200

    tests: make it pass

--- tests/functional/test_user_page.py
+++ tests/functional/test_user_page.py
@@ -1,33 +1,67 @@
 import json
 
-from app.libs.personal_number_helper import get_hash_ids, hash_to_personal_number
+from app import create_app
+from app.libs.personal_number_helper import get_hash_ids
+from tests.conftest import TestVivaProvider
+from tests.unit.libs.classes.providers.test_viva_provider import create_viva_xml_person_cases_mock
+from tests.unit.libs.classes.providers.test_viva_provider import create_viva_xml_person_application_mock
+from tests.unit.libs.classes.providers.test_viva_provider import create_viva_xml_person_case_workflow
 
-# Test user: Petronella Malteskog
+env = 'development'
+
+# Test user
 personal_number = 198602102389
+first_name = 'Petronella'
+last_name = 'Malteskog'
 
 
-def test_user_page(test_client):
+def test_user_page():
     """
     GIVEN a Flask application configured for testing
     WHEN the '/user/[hashId]' page is requested (GET)
     THEN check that the response is a ekb user
     """
 
-    hash_id = get_hash_ids().encode(personal_number)
-    response = test_client.get(f'/user/{hash_id}')
+    provider = TestVivaProvider()
+    provider.PERSONAPPLICATION = lambda USER, PNR, SSI, WORKFLOWID, RETURNAS: create_viva_xml_person_application_mock(
+        mock_data={
+            'start': '2023-04-01',
+            'end': '2023-04-30',
+        })
 
-    data = json.loads(response.data)
+    provider.PERSONCASES = lambda USER, PNR, SYSTEM, RETURNAS: create_viva_xml_person_cases_mock(
+        mock_data={
+            'personal_number': personal_number,
+            'first_name': first_name,
+            'last_name': last_name,
+        })
 
-    expected_personal_number = hash_to_personal_number(hash_id=hash_id)
+    provider.PERSONCASEWORKFLOW = lambda USER, PNR, SSI, MAXWORKFLOWS, RETURNAS: '''
+        <vivadata>
+            <vivacaseworkflows>
+                <workflow></workflow>
+            </vivacaseworkflows>
+        </vivadata>
+    '''
 
-    assert response.status_code == 200
-    assert data == {
-        'type': 'EkbUser',
-        'attributes': {
-            'personalNumber': expected_personal_number,
-            'firstName': 'Petronella',
-            'lastName': 'Malteskog',
-            'cases': [],
-            'persons': [],
-        },
-    }
+    flask_app = create_app(provider=provider, env=env)
+    flask_app.testing = True
+
+    with flask_app.test_client() as test_client:
+
+        hash_id = get_hash_ids().encode(personal_number)
+        response = test_client.get(f'/user/{hash_id}')
+
+        data = json.loads(response.data)
+
+        assert response.status_code == 200
+        assert data == {
+            'type': 'EkbUser',
+            'attributes': {
+                'personalNumber': str(personal_number),
+                'firstName': first_name,
+                'lastName': last_name,
+                'cases': [],
+                'persons': [],
+            },
+        }
