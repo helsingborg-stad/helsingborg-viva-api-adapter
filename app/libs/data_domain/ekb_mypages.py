12c346b22d529560204e40134accbf564be19fde
MrSpex <mail@dannilsson.se>
2023-04-22 08:16:50 +0200

    refactor: simplify typing

--- app/libs/data_domain/ekb_mypages.py
+++ app/libs/data_domain/ekb_mypages.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass, field
-from typing import Dict
 
 
 @dataclass
 class EkbMyPages:
-    cases: Dict[str, str] = field(default_factory=dict)
-    application: Dict[str, str] = field(default_factory=dict)
+    cases: dict = field(default_factory=dict)
+    application: dict = field(default_factory=dict)
