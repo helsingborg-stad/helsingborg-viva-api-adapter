6746c436289b7aa4f92bfa3c8760c8ebc7de6560
MrSpex <mail@dannilsson.se>
2023-04-24 13:24:17 +0200

    fix: get workflow(s)

--- app/routes/my_pages_workflows.py
+++ app/routes/my_pages_workflows.py
@@ -1,7 +1,6 @@
-from typing import Union
 from flask_restful import Resource
 
-from app.libs.providers.ekb_abc_provider import EkbABCProvider
+from app.libs.providers.viva_abc_provider import AbstractVivaProvider
 from app.libs.classes.viva_my_pages import VivaMyPages
 from app.libs.personal_number_helper import hash_to_personal_number
 from app.libs.authenticate_helper import authenticate
@@ -10,13 +9,13 @@ from app.libs.authenticate_helper import authenticate
 class MyPagesWorkflows(Resource):
     method_decorators = [authenticate]
 
-    def __init__(self, provider: Union[EkbABCProvider, None] = None) -> None:
+    def __init__(self, provider: AbstractVivaProvider) -> None:
         self.provider = provider
 
     def get(self, hash_id, workflow_id=None):
         personal_number = hash_to_personal_number(hash_id=hash_id)
-        self.my_pages = VivaMyPages(client=self.provider, user=personal_number)
-
+        self.my_pages = VivaMyPages(
+            client=self.provider.create_client(wsdl_name='MyPages'), user=personal_number)
         self.workflow_id = workflow_id
 
         if not self.workflow_id:
@@ -31,7 +30,7 @@ class MyPagesWorkflows(Resource):
             workflow_list = [workflow_list]
 
         response = {
-            'type': 'getWorkflows',
+            'type': 'workflows',
             'attributes': {
                 'workflows': workflow_list,
             }
@@ -41,7 +40,7 @@ class MyPagesWorkflows(Resource):
 
     def _get_workflow_details(self):
         response = {
-            'type': 'getWorkflowDetials',
+            'type': 'workflow',
             'attributes': {
                 'workflow': self.my_pages.get_workflow(workflow_id=self.workflow_id),
             }
