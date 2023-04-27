531064edb1dfcc38226ccfa2d7f8987e0e3b7428
Dan Nilsson <mail@dannilsson.se>
2023-04-27 12:56:17 +0200

    feat: add viva data domain class (#76)
    * feat: add viva data domain class
    
    * feat: add viva typing
    
    Decision
    
    * feat: add data class - calculation
    
    * fix: add missing properties
    
    calculationpersons
    costs
    
    * feat: add dataclass Payment
    
    * feat: add data class Journal

---
+++ app/libs/classes/viva_data_domain/__init__.py
@@ -0,0 +1,231 @@
+from dataclasses import dataclass
+from typing import Union, List, Optional
+
+
+@dataclass
+class VivaData:
+    status: int
+
+
+@dataclass
+class Ssi:
+    server: str
+    path: str
+    id: str
+
+
+@dataclass
+class Officer:
+    name: str
+    type: str
+    typeenclair: str
+    phone: str
+    mail: str
+    title: str
+
+
+@dataclass
+class Client:
+    pnumber: str
+    fname: str
+    lname: str
+
+
+@dataclass
+class PhoneNumber:
+    type: str
+    number: str
+
+
+@dataclass
+class Person:
+    pnumber: str
+    fname: str
+    lname: str
+    type: str
+    startdate: Union[str, None]
+    enddate: Union[str, None]
+    days: Union[str, None]
+
+
+@dataclass
+class VivaCase:
+    casessi: Ssi
+    idunique: str
+    idenclair: str
+    type: str
+    typeenclair: str
+    opendate: str
+    classifiedassecret: str
+    unit: str
+    team: str
+    officers: List[Officer]
+    client: Client
+    persons: Union[List[Person], None]
+    phonenumbers: Union[List[PhoneNumber], None]
+
+
+@dataclass
+class Application:
+    receiveddate: Union[str, None] = None
+    periodstartdate: Union[str, None] = None
+    periodenddate: Union[str, None] = None
+    otherperiod: Union[str, None] = None
+    requestingcompletion: Union[str, None] = None
+    completiondate: Union[str, None] = None
+    completionreceiveddate: Union[str, None] = None
+    completionsreceived: Union[str, None] = None
+    completions: Union[List[str], None] = None
+    completiondescription: Union[str, None] = None
+    completionduedate: Union[str, None] = None
+    islockedwithoutcompletionreceived: Union[str, None] = None
+    islocked: Union[str, None] = None
+
+
+@dataclass
+class CalculationPerson:
+    daycare: Union[str, None] = None
+    days: Union[str, None] = None
+    home: Union[str, None] = None
+    name: Union[str, None] = None
+    norm: Union[str, None] = None
+    pnumber: Union[str, None] = None
+
+
+@dataclass
+class CalculationCost:
+    actual: Union[str, None] = None
+    approved: Union[str, None] = None
+    note: Union[str, None] = None
+    type: Union[str, None] = None
+
+
+@dataclass
+class CalculationNormPart:
+    actual: Union[str, None] = None
+    note: Union[str, None] = None
+    type: Union[str, None] = None
+
+
+@dataclass
+class Calculation:
+    calculationsum: Union[str, None] = None
+    calculationpersons: Union[List[CalculationPerson], None] = None
+    calculationtype: Union[str, None] = None
+    costs: Union[List[CalculationCost], None] = None
+    costsum: Union[str, None] = None
+    createdby: Union[str, None] = None
+    createddatetime: Union[str, None] = None
+    incomes: Union[str, None] = None
+    incomesum: Union[str, None] = None
+    normgemsum: Union[str, None] = None
+    normsubtotal: Union[str, None] = None
+    normsum: Union[str, None] = None
+    note: Union[str, None] = None
+    periodenddate: Union[str, None] = None
+    periodstartdate: Union[str, None] = None
+    reductions: Union[str, None] = None
+    reductionsum: Union[str, None] = None
+    subject: Union[str, None] = None
+    parentssi: Union[Ssi, None] = None
+    ssi: Union[Ssi, None] = None
+    norm: Union[List[CalculationNormPart], None] = None
+
+
+@dataclass
+class Decision:
+    createdby: Union[str, None] = None
+    createddatetime: Union[str, None] = None
+    amount: Union[str, None] = None
+    author: Union[str, None] = None
+    cause: Union[str, None] = None
+    causepartner: Union[str, None] = None
+    causetext: Union[str, None] = None
+    causetextpartner: Union[str, None] = None
+    code: Union[str, None] = None
+    codetext: Union[str, None] = None
+    createdby: Union[str, None] = None
+    createddatetime: Union[str, None] = None
+    date: Union[str, None] = None
+    explanation: Union[str, None] = None
+    id: Union[str, None] = None
+    purpose: Union[str, None] = None
+    type: Union[str, None] = None
+    typecode: Union[str, None] = None
+
+
+@dataclass
+class DecisionBase:
+    createdby: Union[str, None] = None
+    createddatetime: Union[str, None] = None
+    parentssi: Union[Ssi, None] = None
+    decisions: Union[List[Decision], None] = None
+    periodenddate: Union[str, None] = None
+    periodstartdate: Union[str, None] = None
+    ssi: Union[Ssi, None] = None
+    subject: Union[str, None] = None
+
+
+@dataclass
+class Payment:
+    amount: Union[str, None] = None
+    canceled: Union[str, None] = None
+    createdby: Union[str, None] = None
+    createddatetime: Union[str, None] = None
+    givedate: Union[str, None] = None
+    method: Union[str, None] = None
+    msg1: Union[str, None] = None
+    msg2: Union[str, None] = None
+    paymentid: Union[str, None] = None
+    parentssi: Union[Ssi, None] = None
+    periodenddate: Union[str, None] = None
+    periodstartdate: Union[str, None] = None
+    purpose: Union[str, None] = None
+    receivername: Union[str, None] = None
+    receiverpnumber: Union[str, None] = None
+    ssi: Union[Ssi, None] = None
+    status: Union[str, None] = None
+    subject: Union[str, None] = None
+    type: Union[str, None] = None
+
+
+@dataclass
+class Journal:
+    createdby: Union[str, None] = None
+    createddatetime: Union[str, None] = None
+    eventdate: Union[str, None] = None
+    periodenddate: Union[str, None] = None
+    periodstartdate: Union[str, None] = None
+    responsible: Union[str, None] = None
+    ssi: Union[Ssi, None] = None
+    subject: Union[str, None] = None
+
+
+@dataclass
+class Workflow:
+    workflowid: str
+    application: Application
+    calculations: Optional[List[Calculation]] = None
+    decisions: Optional[List[DecisionBase]] = None
+    payments: Optional[List[Payment]] = None
+    journals: Optional[List[Journal]] = None
+
+
+@dataclass
+class VivaCaseWorkflows(VivaData):
+    vivacaseworkflows: List[Workflow]
+
+
+@dataclass
+class VivaCases(VivaData):
+    vivacases: List[VivaCase]
+
+
+@dataclass
+class VivaPersonCases:
+    vivadata: VivaCases
+
+
+@dataclass
+class VivaPersonCaseWorkflow:
+    vivadata: VivaCaseWorkflows
